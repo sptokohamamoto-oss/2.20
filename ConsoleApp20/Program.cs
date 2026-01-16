4302198f0fb5313f4237e8f443fd3f073c54206d
冬桜子 濱元 <toko0124615@icloud.com>
2026-01-16 14:23:14 +0900

    a

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -1,35 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Security.Cryptography.X509Certificates;
+using System.Reflection.Metadata.Ecma335;
 
-namespace ConsoleAPP20
-{
-    class Program
+ abstract class Employee
     {
-        static void Main()
-        {
-            List<Employee> employees = new List<Employee>
-        {
-        new FullTimeEmployee("E001","山田太郎",8.5),
-        new ContractEmployee("C001","佐藤花子",8.0),
-        new FullTimeEmployee("E002","鈴木一郎",8.0),
-        };
-
-
-            foreach(var item in employees)
-{
-                double wage = item.CalculateDailyWage(item.HoursWorked);
-                int wageInt = (int)wage;
-                Console.WriteLine($"社員ID: {item.Id}, 名前: {item.Name}, 給料: {wageInt}");
-            }
-            
-        }
-    }
-    abstract class Employee
-    {
-        public string Id { get; }
-        public string Name { get; }
-        public double HoursWorked { get; }
+        public string Id;
+        public string Name;
+        public double HoursWorked;
 
         protected Employee(string id, string name, double hoursWorked)
         {
@@ -37,38 +14,54 @@ namespace ConsoleAPP20
             Name = name;
             HoursWorked = hoursWorked;
         }
-        public abstract double CalculateDailyWage(double hoursWorked);
+        public abstract int CalculateDailyWage();
     }
     class FullTimeEmployee : Employee
     {
-        private const double HourlyWage = 1250;
-        private const double OvertimeRate = 1.25;
-        private const double RegularHours = 8.0;
-
         public FullTimeEmployee(string id, string name, double hoursWorked) : base(id, name, hoursWorked)
         {
         }
-        public override double CalculateDailyWage(double hoursWorked)
+        public override int CalculateDailyWage()
         {
-         double regularHours = Math.Min(8.0, HoursWorked);
-         double overtimeHours = Math.Max(0.0, HoursWorked - 8.0);
-
-            return regularHours * 1250 + overtimeHours * 1250 * 1.25;
-
+        int wage = (int)(1250 * 8);
+        if (HoursWorked > 8) 
+        {
+            wage += (int)(1250 * 1.25 * (HoursWorked - 8));
+        }
+        return wage;
         }
     }
     class ContractEmployee : Employee
-    {
-        public const double HourlyWage = 1000;
+{
         public ContractEmployee(string id, string name, double hoursWorked)
             : base(id,name,hoursWorked)
         {
         }
-        public override double CalculateDailyWage(double hoursWorked)
+        public override int CalculateDailyWage()
         {
-            return HoursWorked * 1000;
+        int wage = (int)(1000 * HoursWorked);
+        return wage;
         }
     }
-   
-}
+    class Program
+    {
+        static void Main()
+        {
+            List<Employee> list = new List<Employee>
+        {
+        new FullTimeEmployee("E001","山田太郎",8.5),
+        new ContractEmployee("C001","佐藤花子",8.0),
+        new FullTimeEmployee("E002","鈴木一郎",8.0),
+        };
+
+
+            foreach (var item in list)
+            {
+                double wage = item.CalculateDailyWage();
+                Console.WriteLine($"社員ID: {item.Id}, 名前: {item.Name}, 給料: {wage}");
+            }
+
+        }
+    }
+
 
