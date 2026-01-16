f6171e24d67d0f818a11860fa3cf3b82c7408a7a
冬桜子 濱元 <toko0124615@icloud.com>
2026-01-16 15:48:45 +0900

    a

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -1,67 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Reflection.Metadata.Ecma335;
 
- abstract class Employee
-    {
-        public string Id;
-        public string Name;
-        public double HoursWorked;
-
-        protected Employee(string id, string name, double hoursWorked)
-        {
-            Id = id;
-            Name = name;
-            HoursWorked = hoursWorked;
-        }
-        public abstract int CalculateDailyWage();
-    }
-    class FullTimeEmployee : Employee
-    {
-        public FullTimeEmployee(string id, string name, double hoursWorked) : base(id, name, hoursWorked)
-        {
-        }
-        public override int CalculateDailyWage()
-        {
-        int wage = (int)(1250 * 8);
-        if (HoursWorked > 8) 
-        {
-            wage += (int)(1250 * 1.25 * (HoursWorked - 8));
-        }
-        return wage;
-        }
-    }
-    class ContractEmployee : Employee
+class Program
 {
-        public ContractEmployee(string id, string name, double hoursWorked)
-            : base(id,name,hoursWorked)
-        {
-        }
-        public override int CalculateDailyWage()
-        {
-        int wage = (int)(1000 * HoursWorked);
-        return wage;
-        }
-    }
-    class Program
+    
+    static void Main()
     {
-        static void Main()
+        var employee  = new List<(Employee emp,double hours)>
         {
-            List<Employee> list = new List<Employee>
-        {
-        new FullTimeEmployee("E001","山田太郎",8.5),
-        new ContractEmployee("C001","佐藤花子",8.0),
-        new FullTimeEmployee("E002","鈴木一郎",8.0),
+           ( new FullTimeEmployee("E001", "山田太郎"),8.5),
+           ( new ContractEmployee("C001", "佐藤花子"),8.0),
+           (new FullTimeEmployee("E002", "鈴木一郎"),8.0),
         };
 
 
-            foreach (var item in list)
-            {
-                double wage = item.CalculateDailyWage();
-                Console.WriteLine($"社員ID: {item.Id}, 名前: {item.Name}, 給料: {wage}");
-            }
-
+        foreach (var item in employee)
+        {
+            double wage = item.emp.CalculateDailyWage(item.hours);
+            Console.WriteLine($"社員ID: {item.emp.Id}, 名前: {item.emp.Name}, 給料: {(int)wage}");
         }
     }
+}
 
 
+abstract class Employee
+{
+    public string Id;
+    public string Name;
+
+    protected Employee(string id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+    public abstract double CalculateDailyWage(double HoursWorked);
+}
+class FullTimeEmployee : Employee
+{
+    private const double HourlyWage = 1250;
+    private const double OvertimeRate = 1.25;
+    private const double RegularHours = 8.0;
+
+    public FullTimeEmployee(string id, string name)
+        : base(id, name)
+    {
+    }
+
+    public override double CalculateDailyWage(double HoursWorked)
+    {
+        double regular = Math.Min(HoursWorked, RegularHours);
+        double overtime = Math.Max(0.0, HoursWorked - RegularHours);
+        return regular * HourlyWage + overtime * HourlyWage * OvertimeRate;
+    }
+}
+
+class ContractEmployee : Employee
+{
+    private const double HourlyWage = 1000;
+
+    public ContractEmployee(string id, string name)
+        : base(id, name)
+    {
+    }
+
+    public override double CalculateDailyWage(double HoursWorked)
+    {
+        return HoursWorked * HourlyWage;
+    }
+}
+  
